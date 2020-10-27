ed65384c56877eb3816612cb9bdce9a7eba69dd7
Mohsen Fallahnejad <mohsenfallahnjd@gmail.com>
2020-10-27 18:18:53 +0330

    Initail Commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    {{ data }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+
+  data:() => ({
+    obj: {},
+    data: '',
+  }),
+
+  mounted() {
+    axios.get('https://yasna.team/manage/rewards/stars')
+    .then((res) => {
+      this.obj = res.data;
+    }).finally(() => {
+      this.data = String(this.obj).split('>')[1]
+      .split('<')[0]
+      .split('(').join('{')
+      .split(')').join('}')
+      .split('[').join('')
+      .split(']').join('')
+      .split('Array').join('')
+      .split('items:').join('')
+      .split('=&gt;').join(':');
+
+      this.data = this.data.split(this.data.split('{')[0]).join('');
+
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
